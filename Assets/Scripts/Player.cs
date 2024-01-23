30ceb71b80e517966dc236afddc5bc1d4c1d47c4
21-Harriz <muhammadharriz27@gmail.com>
2024-01-23 12:23:41 +0800

    New Unity Project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public float jumpForce;
+    public Rigidbody rb;
+
+    public int score;
+    public Text ScoreText;
 
     void Start()
     {
@@ -15,6 +23,41 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
             thisAnimation.Play();
+        }
+        if(transform.position.y >= 3.59)
+        {
+            transform.position = new Vector3(transform.position.x, 3.59f, transform.position.z);
+        }
+        else if (transform.position.y <= -4.41)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+
+        if (score >= 100)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+            
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("PassObstacle"))
+        {
+            score+=10;
+            ScoreText.GetComponent<Text>().text = "SCORE : " + score;
+        }
     }
 }
